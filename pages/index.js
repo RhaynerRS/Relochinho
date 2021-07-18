1256a971c4f93f3acc657434a690a760920fe005
rhayner <rhayner3301@gmail.com>
2021-07-18 00:00:28 -0300

    pomodoro update - (17/07/2021)

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react'
-import styled from 'styled-components';
 import { useState } from 'react';
 import MainGrid from '../src/components/MainGrid'
 import TimeUnit from '../src/components/TimeUnit'
@@ -8,15 +7,15 @@ import Body from '../src/components/Body'
 
 //variaveis mutaveis
 let status = 'study'
-let itsPaused=false
-let pauseLabel='pause'
+let itsPaused = false
+let pauseLabel = 'pause'
 
-if (itsPaused){
-  pauseLabel='play'
-}else{
-  pauseLabel='pause'
+//pause
+if (itsPaused) {
+  pauseLabel = 'play'
+} else {
+  pauseLabel = 'pause'
 }
-
 
 export default function Home() {
   //variaveis inicio
@@ -26,14 +25,14 @@ export default function Home() {
   const [minutos, setMinutos] = useState(0)
 
   //tempo total
-  const [totalSegundos,setTotalSegundos] = useState(0)
-  const [totalMinutos,setTotalMinutos] = useState(0)
+  const [totalSegundos, setTotalSegundos] = useState(0)
+  const [totalMinutos, setTotalMinutos] = useState(0)
   //variaveis fim
 
   //contador de segundos totais inicio
   useEffect(() => {
     const timer = setInterval(() => {
-      if (!itsPaused && status == 'study'){
+      if (!itsPaused && status == 'study') {
         setTotalSegundos(totalSegundos => totalSegundos + 1);
       }
     }, 1000);
@@ -44,10 +43,7 @@ export default function Home() {
   //contador de segundos totais fim
 
   //minutos totais
-  if (totalSegundos>=60 && status == 'study'){
-    setTotalMinutos(totalMinutos=>totalMinutos+1)
-    setTotalSegundos(0)
-  }
+  if (totalSegundos >= 60 && status == 'study') {setTotalMinutos(totalMinutos => totalMinutos + 1);setTotalSegundos(0)}
 
   //horas totais
 
@@ -55,7 +51,7 @@ export default function Home() {
   //contador de segundos inicio
   useEffect(() => {
     const timer = setInterval(() => {
-      if (!itsPaused){
+      if (!itsPaused) {
         if (status == 'study') {
           setSegundos(segundos => segundos + 1);
         } else {
@@ -72,55 +68,36 @@ export default function Home() {
   //controlador dos minutos inicio
 
   //count up
-  if (segundos >= 60 && status == 'study') {
-    setMinutos(minutos => minutos + 1)
-    setSegundos(0)
-  }
+  if (segundos >= 60 && status == 'study') { setMinutos(minutos => minutos + 1); setSegundos(0) }
   //count down
-  else if (segundos == 0 && status == 'break') {
-    setMinutos(minutos => minutos - 1)
-    setSegundos(59)
-  }
+  else if (segundos == 0 && status == 'break') { setMinutos(minutos => minutos - 1); setSegundos(59) }
 
   //controlador dos minutos fim
 
   //horario de estudo
-  if (minutos == 25 && status == 'study') {
-    status = 'break'
-    setSegundos(59)
-    setMinutos(4)
-  }
+  if (minutos == 25 && status == 'study') { status = 'break'; setSegundos(59); setMinutos(4); }
 
   //horario de pausa
-  else if (minutos == 0 && status == 'break') {
-    status = 'study'
-    setSegundos(0)
-    setMinutos(0)
-  }
+  else if (minutos == 0 && status == 'break') { status = 'study'; setSegundos(0); setMinutos(0) }
 
   //padronizar os numeros
-  let segundosTela=segundos
+  let segundosTela = segundos
+  let minutosTela = minutos
+  let totalMinutosTela = totalMinutos
+  let totalSegundosTela = totalSegundos
 
-  let minutosTela=minutos
-
-
-  if (minutos<10){
-    minutosTela="0"+minutos
-  }
-  if (segundos<10){
-    segundosTela="0"+segundos
-  }
-  
-
-
+  if (totalMinutos < 10) { totalMinutosTela = '0' + totalMinutos }
+  if (totalSegundos < 10) { totalSegundosTela = '0' + totalSegundos }
+  if (minutos < 10) { minutosTela = "0" + minutos }
+  if (segundos < 10) { segundosTela = "0" + segundos }
 
   return (<>
     <Body>
       <MainGrid>
-      <TimeUnit>{minutosTela}:{segundosTela}</TimeUnit>
-        
+        <TimeUnit>{minutosTela}:{segundosTela}</TimeUnit>
+
       </MainGrid>
-      <h3 style={{textAlign: 'center'}}>total study time: {totalMinutos} : {totalSegundos}</h3>
+      <h3 style={{ textAlign: 'center' }}>total study time: {totalMinutosTela} : {totalSegundosTela}</h3>
       <MainGrid>
         <ButtonPomodoro onClick={
           () => {
@@ -132,18 +109,18 @@ export default function Home() {
         }>reset</ButtonPomodoro>
 
         <ButtonPomodoro onClick={
-            ()=>{
-              if (!itsPaused){
-                itsPaused=true;
-              }
-              else{
-                itsPaused=false;
-              }
+          () => {
+            if (!itsPaused) {
+              itsPaused = true;
             }
-          }>{pauseLabel}</ButtonPomodoro>
+            else {
+              itsPaused = false;
+            }
+          }
+        }>{pauseLabel}</ButtonPomodoro>
       </MainGrid>
-     
-      <h1 style={{textAlign:'center'}}>its time to {status}</h1>
+
+      <h1 style={{ textAlign: 'center' }}>its time to {status}</h1>
     </Body>
   </>)
 }
