a00acca05c3d35c73f59db7098a30dd9bd33ce21
Rhayener Rossmann <rhayner3301@gmail.com>
2021-07-20 16:13:19 -0300

    pomodoro update - (20/07/2021)

---
+++ src/config/firebase-config.js
@@ -0,0 +1,22 @@
+import firebase from "firebase"
+//
+import isNode from "detect-node"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC4tAlMCffKvgV04VsOaupHGbNJLpClyl8",
+    authDomain: "relochinho.firebaseapp.com",
+    projectId: "relochinho",
+    storageBucket: "relochinho.appspot.com",
+    messagingSenderId: "324691041709",
+    appId: "1:324691041709:web:0d5b951154c9b576b65524",
+    measurementId: "G-PGVQW4E4ZL"
+  };
+  if (firebase.apps.length === 0) {
+    firebase.initializeApp(firebaseConfig);
+  }
+  if(!isNode){
+    firebase.analytics()
+  }
+
+
+export default firebase;
---
+++ src/service/auth.js
@@ -0,0 +1,12 @@
+import firebase from "/src/config/firebase-config";
+
+const socialMediaAuth=(provider)=>{
+    return firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then((res)=>{
+        return res.user;
+    }).catch((err)=>{return err})
+}
+
+export default socialMediaAuth;
